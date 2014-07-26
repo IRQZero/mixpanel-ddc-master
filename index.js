2d817ead772b16d4bcd480613cc84fd1451e98be
Gabriel Hernandez <gabriel.hernandez@lookout.com>
2014-07-26 15:23:50 -0700

    Adding some front end components

--- index.js
+++ index.js
@@ -1,24 +1,71 @@
 (function(){
   var express = require("express"),
-    nano = require("nano"),
+    http = require("http"),
+    nano = require("nano")("http://localhost:5984"),
     socket = require("socket.io"),
-    nodeStatic = require("serve-static");
+    nodeStatic = require("serve-static"),
+    bodyParser = require("body-parser");
 
-  var app = express();
+  var app = express(),
+    server = http.Server(app),
+    io = socket(server),
+    devices = {},
+    clients = [];
 
   app.get('register', function(req, res){
-    
+    var macAddress = req.body.macAddress,
+      device = devices[macAddress];
+
+    if (device == null) {
+      devices[macAddress] = req.body;
+      device = devices[macAddress];
+    }
+
+    if (device.location == null) {
+      res.json({status: "wait"});
+    } else {
+      res.json(device);
+    }
   });
 
   app.get('location', function(req, res){
 
   });
 
-  app.get('user', function(req, res){
+  app.post('user', function(req, res){
 
   });
 
   app.use(nodeStatic(__dirname + "/public"));
-  app.listen(8000);
+  app.use(bodyParser.json());
+
+  io.on('connection', function(socket){
+    clients.push(socket);
+    socket.emit("welcome", {});
+    socket.on('ido', function(job){
+      switch( job ){
+        case 'register':
+          socket.on('register-device', function(device, location){
+            devices[device].location = location;
+          });
+          break;
+        case 'bar':
+          socket.on('order-drink', function(user, drink){
+
+          });
+          break;
+        case 'coatcheck':
+          socket.on('check-coat', function(user, coat){
+
+          });
+          break;
+        default:
+          throw new Error('Invalid job sent');
+          break;
+      }
+    });
+  });
+
+  server.listen(8000);
 
 })();
