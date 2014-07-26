23f6af2bd0739992c585c65429045bfb4672f3f7
Gabriel Hernandez <gabriel.hernandez@lookout.com>
2014-07-26 14:19:06 -0700

    add routes

--- index.js
+++ index.js
@@ -2,9 +2,23 @@
   var express = require("express"),
     nano = require("nano"),
     socket = require("socket.io"),
-    nodeStatic = require("express-static");
+    nodeStatic = require("serve-static");
 
   var app = express();
 
+  app.get('register', function(req, res){
+    
+  });
+
+  app.get('location', function(req, res){
+
+  });
+
+  app.get('user', function(req, res){
+
+  });
+
+  app.use(nodeStatic(__dirname + "/public"));
   app.listen(8000);
-});
+
+})();
